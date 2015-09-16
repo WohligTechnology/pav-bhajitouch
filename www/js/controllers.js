5db31ba2a9873d13f6a65c9d13cf5fb8b5c2f40b
Rohan <gadarohan17@gmail.com>
2015-09-16 17:56:27 +0530

    Added searchbar,fixed uppermarging and no-header

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,21 +1,23 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
- 
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    $scope.searchbar = false;
+    $scope.searchToggle = function () {
+        $scope.searchbar = $scope.searchbar === true ? false : true;
+    };
 })
 
-.controller('HomeCtrl', function($scope) {
-         $scope.slides = [{
-            image: "img/slider/1.jpg",
-           
-    },  {
-            image: "img/slider/2.jpg",
-           
+.controller('HomeCtrl', function ($scope) {
+    $scope.slides = [{
+        image: "img/slider/1.jpg",
+
     }, {
-            image: "img/slider/3.jpg",
-           
+        image: "img/slider/2.jpg",
+
+    }, {
+        image: "img/slider/3.jpg",
+
     }];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('PlaylistCtrl', function ($scope, $stateParams) {});
