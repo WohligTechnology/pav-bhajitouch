39376c29508f8efa305238fd986d74eb6ff027a3
Rohan <gadarohan17@gmail.com>
2015-09-18 14:00:38 +0530

    brands!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,10 +15,49 @@ angular.module('starter.controllers', [])
         image: "img/slider/2.jpg",
 
     }, {
-        image: "img/slider/3.jpg",
+        image: "img/slider/.png",
 
     }];
 })
 
 .controller('PlaylistCtrl', function ($scope, $stateParams) {})
-.controller('BrandsCtrl', function ($scope, $stateParams) {});
+    .controller('BrandsCtrl', function ($scope, $stateParams) {
+        $scope.brands = [{
+            image: "img/brands/acmemade.jpeg",
+    }, {
+            image: "img/brands/Adidas.png",
+
+    }, {
+            image: "img/brands/adonit.png",
+
+    }, {
+            image: "img/brands/apple.png",
+
+    }, {
+            image: "img/brands/autodrive.png",
+
+    }, {
+            image: "img/brands/autodrive.png",
+
+    }, {
+            image: "img/brands/beats.png",
+
+    }, {
+            image: "img/brands/dell.png",
+
+    }, {
+            image: "img/brands/gstarraw.png",
+
+    }, {
+            image: "img/brands/dolcegabbana.jpg",
+
+    }, {
+            image: "img/brands/gas.jpg",
+
+            }, {
+    image: "img/brands/hp.png",
+
+}]; 
+    $scope.brands=_.chunk($scope.brands,3);
+    console.log($scope.brands);
+    });
